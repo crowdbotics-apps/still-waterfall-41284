cfda0c94ff095dc60d0bd88ad5654faffe840c77
Crowdbotics <team@crowdbotics.com>
2023-06-06 11:04:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "still-waterfall-41284",
+  url: "https://still-waterfall-41284.botics.co",
+  api: "https://still-waterfall-41284.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
